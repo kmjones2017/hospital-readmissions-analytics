0ba196d51edbee0f7620ef6708e496760acc9ed9
Kayla Jones <kmjoness2017@gmail.com>
2026-05-11 15:23:00 +0000

    Finalized configs for seeds in dbt_project.yml and _seeds.yml for proper ingestion into warehouse

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,13 +1,12 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
-    {%- if node.resource_type == 'seed' -%}
-        {{ custom_schema_name | trim }}
+    {%- if custom_schema_name is none -%}
 
-    {%- elif custom_schema_name is none -%}
         {{ target.schema }}
 
     {%- else -%}
-        {{ target.schema }}_{{ custom_schema_name | trim }}
+
+        {{ custom_schema_name | trim }}
 
     {%- endif -%}
 
